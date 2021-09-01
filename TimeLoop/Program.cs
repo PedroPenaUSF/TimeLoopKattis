28ca8f67a61027509cef16dfeb6cfeb4526473bf
PedroAPena <pena3@usf.edu>
2021-09-01 17:56:33 -0400

    TimeLoop Commit 1

---
+++ TimeLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine(i + " Abracadabra");
+            }
+        }
+    }
+}
